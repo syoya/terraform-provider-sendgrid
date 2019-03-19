219224178ff978efccab536b41930b8476544c64
PyYoshi <myoshi321go@gmail.com>
2019-03-19 12:31:39 +0900

    set ForceNew to false

--- sendgrid/resource_sendgrid_template_version.go
+++ sendgrid/resource_sendgrid_template_version.go
@@ -49,7 +49,7 @@ func resourceSendgridTemplateVersion() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 				Computed: false,
-				ForceNew: true,
+				ForceNew: false,
 				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
 					localSha256Sum := ""
 
@@ -75,7 +75,7 @@ func resourceSendgridTemplateVersion() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 				Computed: false,
-				ForceNew: true,
+				ForceNew: false,
 				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
 					localSha256Sum := ""
 
